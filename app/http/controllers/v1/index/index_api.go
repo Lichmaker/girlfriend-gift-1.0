0f5683dc388c6db62eda3c5f5ce52e0019ccf04f
wuguozhang <lich.wu2014@gmail.com>
2021-10-21 15:01:30 +0800

    :construction: 允许跨域

--- app/http/controllers/v1/index/index_api.go
+++ app/http/controllers/v1/index/index_api.go
@@ -12,9 +12,11 @@ type IndexApi struct {
 }
 
 func (i *IndexApi) Get(c *gin.Context) {
-	url := picker.Do(time.Now().Format("2006-01-02"))
+	c.Header("Access-Control-Allow-Origin", "*")
+	url, modDate := picker.Do(time.Now().Format("2006-01-02"))
 
 	c.JSON(http.StatusOK, gin.H{
 		"url": url,
+		"mod_date": modDate,
 	})
 }
--- pkg/picker/picker.go
+++ pkg/picker/picker.go
@@ -3,13 +3,15 @@ package picker
 import (
 	"lichmaker/girlfriend-gift-1/app/models/schedule"
 	"lichmaker/girlfriend-gift-1/pkg/oss_helper"
+	"time"
 )
 
-func Do(date string) (string) {
+func Do(date string) (string, string) {
 	r, err := schedule.GetRandom(date)
 	if err != nil {
-		return ""
+		return "", ""
 	}
-	
-	return oss_helper.GetUrl(r.Path, int64(600))
+	dateTime, _ := time.Parse(time.RFC3339, r.ModDate)
+	dateString := dateTime.Format("2006-01-02")
+	return oss_helper.GetUrl(r.Path, int64(600)), dateString
 }
